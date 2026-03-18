9dd331cf3d9e04fb090ff3f70f1b824f2365347a
love-mr <nowitsme972@gmail.com>
2026-03-18 13:52:54 +0530

    Update views.py

--- vaccination/views.py
+++ vaccination/views.py
@@ -1,3 +1,4 @@
+sakthi
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required, user_passes_test
